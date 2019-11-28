4ad3e2abb7a397e742fec7d9c776f8d0781c0938
Zaid Al-Omari <zaid@saal.ai>
2019-11-28 13:48:26 +0400

    decouple consumers from producers using redis-bus

--- src/services/CommandsProcessor.ts
+++ src/services/CommandsProcessor.ts
@@ -1,29 +1,55 @@
 import { KafkaService } from './KafkaService';
 import config from '../config';
 import { Func } from 'continuation-local-storage';
-
+import { createRedisBus } from '@saal-oryx/message-bus';
+import nanoid = require('nanoid');
 export class CommandsProcessor {
 
 
   protected _commandsMap = new Map<string, (err?: Error, result?: any) => void>();
+  protected _messageBus = createRedisBus(nanoid(10), {
+    host: config.redisHost,
+    port: config.redisPort
+  });
 
   constructor(protected _kafkaService: KafkaService) {
-
+    this._messageBus.on<ICommandResult>('resolveCommand', async (event, _) => {
+      const callback = this._commandsMap.get(event.data.eventKey);
+      if (!callback) return;
+      return callback(undefined, event.data.result);
+    });
+    this._messageBus.on<ICommandResult>('rejectCommand', async (event, _) => {
+      const callback = this._commandsMap.get(event.data.eventKey);
+      if (!callback) return;
+      return callback(event.data.error);
+    });
   }
 
   async resolveCommand<R>(eventKey: string, result: R) {
-    const callback = this._commandsMap.get(eventKey);
-    if (!callback) return;
-    return callback(undefined, result);
+    this._messageBus.publish(<any>{ name: 'resolveCommand', data: { eventKey, result } });
+
   }
 
   async rejectCommand(eventKey: string, error: Error) {
-    const callback = this._commandsMap.get(eventKey);
-    if (!callback) return;
-    return callback(error);
+    this._messageBus.publish(<any>{ name: 'rejectCommand', data: { eventKey, error } });
   }
 
   async sendCommand(serviceName: string, proccessingFunction: Func<Promise<any>>, ...args: any[]): Promise<{ done: boolean, data: any }> {
+    const event = await this.sendCommandAsync(serviceName, proccessingFunction);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        this._commandsMap.delete(event.key);
+        return resolve({ done: false, data: args });
+      }, config.comamndsTimeout);
+      this._commandsMap.set(event.key, (err, result) => {
+        this._commandsMap.delete(event.key);
+        if (err) return reject(err);
+        return resolve({ done: true, data: result });
+      });
+    });
+  }
+
+  async sendCommandAsync(serviceName: string, proccessingFunction: Func<Promise<any>>, ...args: any[]) {
     const commandKey = this._kafkaService.getNewKey();
     const event = {
       event: `${proccessingFunction.name}_${serviceName}`,
@@ -33,16 +59,12 @@ export class CommandsProcessor {
       key: commandKey
     };
     await this._kafkaService.send(config.kafkaCommandsTopic, event);
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        this._commandsMap.delete(commandKey);
-        return resolve({ done: false, data: args });
-      }, config.comamndsTimeout);
-      this._commandsMap.set(commandKey, (err, result) => {
-        this._commandsMap.delete(commandKey);
-        if (err) return reject(err);
-        return resolve({ done: true, data: result });
-      });
-    });
+    return event;
   }
+}
+
+interface ICommandResult {
+  eventKey: string;
+  result?: any;
+  error?: any;
 }
