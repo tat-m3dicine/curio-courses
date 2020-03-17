4969c54af34fe6a2e0c640f3e7ca68130a138405
Omar Einea <omar@saal.ai>
2020-03-17 13:21:08 +0400

    CW-5455: commit before sending enrollement updates in doSwitchUsers

--- debug.sh
+++ debug.sh
@@ -1,5 +1,5 @@
 echo Installing Dependancies and Compiling Project
 # npm config set https-proxy http://192.168.42.129:1080
-npm install --registry https://npr.saal.ai
+# npm install --registry https://npr.saal.ai
 # npm install
 npm run dev
